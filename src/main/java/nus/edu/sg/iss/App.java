6fdab5f89cc9adaef33edcb42531c21d2908e10a
Bryan Chua <Bryanchua.1994@gmail.com>
2023-04-17 11:42:13 +0800

    initial commit

---
+++ src/main/java/nus/edu/sg/iss/App.java
@@ -0,0 +1,13 @@
+package nus.edu.sg.iss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
